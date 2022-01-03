d53045878fa26ecbe2b5494ca71583e9a2a63d4c
Abhilash <Abhilash>
2022-01-03 21:35:01 +0530

    miniumnpath

--- src/MinimunPath.java
+++ src/MinimunPath.java
@@ -10,7 +10,7 @@ public class MinimunPath {
 
 
 
-    public static int minPathSum(int[][] grid) {
+    public int minPathSum(int[][] grid) {
 
 
 
@@ -19,22 +19,17 @@ public class MinimunPath {
         int n=grid[0].length;
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
-                int x=Integer.MAX_VALUE;
-                int y=Integer.MAX_VALUE;
                 if(i==m-1&&j==n-1)
                     continue;
-                if((i+1)<m)
-                    x=grid[i+1][j];
-
-                if((j+1)<n)
-                    y=grid[i][j+1];
-
-
-                grid[i][j]=grid[i][j]+Math.min(x,y);
-                System.out.print("grid"+i+j+" "+grid[i][j]+"     ");
+                else if(i==m-1&&j!=n-1)
+                    grid[i][j]+=grid[i][j+1];
+                else if(j==n-1&&i!=m-1)
+                    grid[i][j]+=grid[i+1][j];
+                else
+                    grid[i][j]+=Math.min(grid[i][j+1],grid[i+1][j]);
 
             }
-            System.out.println();
+
         }
         return grid[0][0];
 
